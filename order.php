dc1484c28e95618ef011e4a1dfa76d0050c343ee
maxeg291 <56290453+maxeg291@users.noreply.github.com>
2019-12-16 18:37:50 +0300

    Add files via upload

--- order.php
+++ order.php
@@ -2,40 +2,42 @@
 <head>
   <meta charset="utf-8">
   <title> Kengurumi </title>
-  <link rel="stylesheet" href="lifestyle.css">
+  
   <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
 </head>
 <body>
   <div class="top" align="center">
     <p><img src="images/logo.png" alt="Логотип" style="width:100px; margin-top:40px;">
-    <a rel="nofollow" href="https://www.instagram.com/kengyrumi/" style="padding: 10px 5% 10px;">
+    <a rel="nofollow" href="https://www.instagram.com/kengyrumi/" style="padding: 10px 5% 10px;width:30px">
       <img src="images\instalogo.png" alt="instagram"style="width:20px; margin-top:40px;"></a>
-    <a rel="nofollow" href="https://vk.com/kengyrumi" style="padding: 10px 5% 10px;">
+    <a rel="nofollow" href="https://vk.com/kengyrumi" style="padding: 10px 5% 10px;width:30px">
       <img src="images\vklogo.png" alt="vk" style="width:20px;"></a>
     <a href="tel:+79653691363" style="padding: 10px 5% 10px;"> +7 (965) 369 13 63 </a>
-    <a href="basket.php" style="padding: 10px 5% 10px;">
-      <img src="images\basketimg.png" style="width:20px;"></a></p>
+    <img src="images/logo.png" alt="Логотип" style="width:100px; margin-top:40px;"></p>
   </div>
-  <h1 align="center"> Life </h1>
-  <div class="menu" align=center>
+  <div style="margin-left:475px;" >
+    <pre style="font-size:15px;font-family:arial;"> Мы в Instagram             Мы во Вконтакте                             Позвонить нам</pre>
+  </div>
+  <h1 style="text-align:center;font-size:50px;color:red;font-family:segoe print;"> Оформить заказ </h1>
+  <div class="menu" align=center style="background-color:pink;width:100%;height:15%;margin-left:0px;font-size:20px;">
       <?php if($_COOKIE['login']==''): ?>
         <a href="auth.php" style="padding: 10px 5% 10px;">Зарегистрироваться/Войти</a>
         <?php elseif(($_COOKIE['login']!='') && ($_COOKIE['admin']==1)): ?>
-          <div width="20px"> Вы зашли под именем <?=$_COOKIE['login']?> как администратор. <a href="exit.php"> Выйти </a></div>
+          <p> Вы зашли под именем <?=$_COOKIE['login']?> как администратор. <a href="exit.php"> Выйти </a></p>
         <?php else: ?>
-          <div width="20px"> Вы зашли под именем <?=$_COOKIE['login']?>. <a href="exit.php"> Выйти </a></div>
+          <div width="20px" style="padding-bottom:5px"> Вы зашли под именем <?=$_COOKIE['login']?>. <a href="exit.php"> Выйти </a></div>
       <?php endif; ?>
         <a href="life.php" style="padding: 10px 5% 10px;">Life</a> 
       <a href="catalog.php" style="padding: 10px 5% 10px;"> Модели и размеры </a>
       <a href="feedback.php" style="padding: 10px 5% 10px;"> Отзывы </a>
       <a href="order.php" style="padding: 10px 5% 10px;"> Оформить заказ</a>
     </div>
-  <div class="main" style="text-align:center">
+  <div class="main">
     <?php 
      error_reporting(0);
      if($_COOKIE['login'] == ''):
      ?>
-   <h3 style="color:red;"><a href="auth.php" style="color:#81BEF7;">Войдите</a> или <a href="reg.php" style="color:#81BEF7;">зарегистрируйтесь</a>, чтобы оставить отзыв!</p>
+   <h3 style="color:red;"><a href="auth.php" style="color:#81BEF7;">Войдите</a> или <a href="reg.php" style="color:#81BEF7;">зарегистрируйтесь</a>, чтобы оформить заказ!</p>
     <?php elseif(($_COOKIE['login']!='')&&($_COOKIE['admin']!=1)): ?>
   <h3> Таблица размеров </h3>
     <table style="text-align:center;">
@@ -48,7 +50,7 @@
       <tr><td>175-185cм</td><td>XL</td></tr>
     </table>
    <p> Выберите модели, которые желаете приобрести, и их размеры </p>
-   <form action="orderscript.php" method="POST">
+   <form action="orderscript.php" method="POST" align='center'>
     <p>Модель: 
      <select name="model" size="1">
 	 <?php
@@ -79,7 +81,7 @@ mysqli_close($mysql);
    <?php else: ?>
    <?php
   $mysql = new mysqli('localhost','root','','site');
-  $query ="SELECT `model`,`size`,`email`,`login` FROM `orders`";
+  $query ="SELECT `id`,`model`,`size`,`email`,`login` FROM `orders`";
  
   $result = mysqli_query($mysql, $query); 
   if($result)
@@ -87,9 +89,10 @@ mysqli_close($mysql);
     while($rows = $result->fetch_assoc()){
     echo "<table>";
     echo "<tr>";
+    echo "<td style='color:red'><b><em>".$rows["id"].")</em></b></td>";
     echo "<td><b>".$rows["login"].":</b></td>";
-        echo "<td>".$rows["model"]."</td>";
-        echo "<td>".$rows["size"]."</td>";
+        echo "<td style='color:blue'><b>".$rows["model"]."</b></td>";
+        echo "<td style='color:blue'><b>".$rows["size"]."</b></td>";
         echo "<td>".$rows["email"]."</td>";
     echo "</tr>"; 
         echo "</table>";        
@@ -97,6 +100,7 @@ mysqli_close($mysql);
     mysqli_free_result($result);
 mysqli_close($mysql);
 ?>
+<a href="deleteorder.php">Удалить заказ</a>
 <?php endif; ?>
   </div>
  </body>
